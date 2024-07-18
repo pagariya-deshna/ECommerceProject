45c37e35b8c5ed597de7b7b46c92dd6473847f98
Deshna <deshnapagariya.ngd@gmail.com>
2024-07-18 14:37:24 +0530

    removed print message

--- src/main/java/com/ecommerce/project/ECommerceProjectApplication.java
+++ src/main/java/com/ecommerce/project/ECommerceProjectApplication.java
@@ -17,7 +17,6 @@ public class ECommerceProjectApplication {
 
     public static void main(String[] args) {
     	SpringApplication.run(ECommerceProjectApplication.class, args);  
-    	System.out.println();
 }
 
 }
